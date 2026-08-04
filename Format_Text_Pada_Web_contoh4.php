0e8f92bdba0aa69e945be574f828d99d17b4a1f5
brenzz-star <brenzz706@gmail.com>
2026-08-04 08:56:08 +0700

    Initial commit

---
+++ Format_Text_Pada_Web_contoh4.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Format Text</title>
+
+    <style type="text/css">
+        p.besar {text-transform: uppercase;}
+        p.kecil{text-transform: lowercase;}
+    </style>
+
+</head>
+
+<body>
+    <p class="besar"> Pengubahan kedalam grup besar</p>
+    <p class="kecil"> PENGUBAHAN KEDALAM GRUP KECIL</p>
+</body>
+</html>
